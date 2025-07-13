45e88b349accd4f417f20f60c8398870cef9c657
Maikel van de Lisdonk <maikel@devhelpr.com>
2025-07-13 14:50:31 +0200

    Refactor Netlify deployment function to use btoa for base64 encoding
    - Updated the deployWithNetlify function to utilize btoa for encoding JSON content to base64, ensuring compatibility with browser environments.

--- apps/prompttoform/src/app/utils/netlify-deploy.ts
+++ apps/prompttoform/src/app/utils/netlify-deploy.ts
@@ -17,7 +17,7 @@ export const deployWithNetlify = (json: string) => {
       body: JSON.stringify({
         netlifyAccessToken: netlifyAccessToken,
         netlifySiteId: netlifySiteId,
-        zipContents: new Buffer(json).toString('base64'),
+        zipContents: btoa(json),
       }),
     })
       .then((response) => response.json())
